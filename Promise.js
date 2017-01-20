ea6276e0d40665f53994a01319f42fa2af055b3a
ICO <cash@oh-gaming.com>
2017-01-20 12:10:39 +0800

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,10 +1,10 @@
-1. Declare a Function
+/*1. Declare a Function
 2. return new Promise
 3. new Promise receive a callback and the callback need two argument
 4. that two arguments are resolve and reject respectively
 5. when the callback is work and done use resolve function if error or fail use reject
 6. simple code 
- 
+ */
 function asyncFunction() {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
@@ -20,7 +20,7 @@ asyncFunction().then(function (value) {
 });
 or write it as follows
 asyncFunction().then(console.log).catch(console.log);
-
+/*
 instance of Promise have 3 state
 "has-resolution" - Fulfilled
 當你成功在instance 的callback 成功使用resolve( )時。
@@ -41,3 +41,4 @@ Promise.prototype.onRejected
 到時用async-await寫非同步將變的沒有難度可言(雖然目前也沒多難就是了)
 只剩寫底層api的人會需要學Promise和其運作原理
 
+*/
